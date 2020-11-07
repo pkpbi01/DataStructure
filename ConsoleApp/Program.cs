40cadbf16ab3ba5684a51e371abc9affa0a154c5
first <pkpbi01@mail.ru>
2020-11-07 20:05:50 +0300

    start

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DataStructure;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList {};
+            list.AddToEnd(1);
+            list.AddToEnd(3);
+            list.AddToEnd(4);
+            list.AddTo(1, 2);
+            list.AddToBiginning(0);
+            list.AddToBiginning(-1);
+            list.AddTo(0, -2);
+            Console.WriteLine(list.Lenght);
+            list.PrintList();
+        }
+    }
+}
---
+++ DataStructure/ArrayList.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace DataStructure
+{
+    public class ArrayList
+    {
+        public int Lenght { get; private set; }
+
+        private int[] _array;
+        private int _TrueLenght
+        {
+            get
+            {
+                return _array.Length;
+            }
+        }
+
+        public ArrayList()
+        {
+            _array = new int[9];
+            Lenght = 0;
+        }
+
+        public void AddToEnd(int value)
+        {
+            if (_TrueLenght <= Lenght)
+            {
+                IncreaseLenght();
+            }
+            _array[Lenght] = value;
+            Lenght++;
+        }
+
+        public void AddToBiginning(int value)
+        {
+            if (_TrueLenght <= Lenght)
+            {
+                IncreaseLenght();
+            }
+            for(int i = Lenght; i >= 0; i--)
+            {
+                _array[i + 1] = _array[i];
+            }
+            _array[0] = value;
+            Lenght++;
+        }
+
+        public void AddTo(int idx, int value)
+        {
+            if (_TrueLenght <= Lenght)
+            {
+                IncreaseLenght();
+            }
+            for (int i = Lenght; i >= idx; i--)
+            {
+                _array[i + 1] = _array[i];
+            }
+            _array[idx] = value;
+            Lenght++;
+        }
+
+        public void PrintList()
+        {
+            for(int i = 0; i < Lenght; i++)
+            {
+                Console.Write($"{_array[i]} ");
+            }
+        }
+
+
+
+        private void IncreaseLenght(int number = 1)
+        {
+            int newLenght = _TrueLenght;
+            while (newLenght <= Lenght + number)
+            {
+                newLenght = (int)(newLenght * 1.33 + 1);
+            }
+
+            int[] newArray = new int[newLenght];
+            Array.Copy(_array, newArray, _TrueLenght);
+
+            _array = newArray;
+        }
+
+        public override bool Equals(object obj)
+        {
+            ArrayList arrayList = (ArrayList)obj;
+
+            if (Lenght != arrayList.Lenght)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < Lenght; i++)
+                {
+                    if (_array[i] != arrayList._array[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
